52571b533313a6eba606067d3a4e2dea500db742
Santiago Rios Valero <riossanval@gmail.com>
2024-05-06 11:40:04 -0500

    [ADD] axios functions

--- src/services/prodcts.ts
+++ src/services/prodcts.ts
@@ -1,6 +1,29 @@
-import { callGetHttpAdapter } from "./axios/CallHttpAdapter";
+import { callDeleteHttpAdapter, callGetHttpAdapter, callPostHttpAdapter, callPutHttpAdapter } from "./axios/CallHttpAdapter";
+import { FakeProduct } from '../types/fakeProduct';
 
-  export async function getProducts(): Promise<any> {
-    const response = await callGetHttpAdapter("https://fakestoreapi.com/products", (data) => data);
-    return response;
-  }
+const baseUrl = "https://fakestoreapi.com";
+
+export async function getProducts(): Promise<any> {
+  const response = await callGetHttpAdapter(`${baseUrl}/products`, (data) => data);
+  return response as FakeProduct[];
+}
+
+export async function getProduct(id: string): Promise<any> {
+  const response = await callGetHttpAdapter(`${baseUrl}/products/${id}`, (data) => data);
+  return response as FakeProduct;
+}
+
+export async function updateProduct(id: string, fakeProduct: FakeProduct): Promise<any> {
+  const response = await callPutHttpAdapter(`${baseUrl}/products/${id}`, fakeProduct);
+  return response;
+}
+
+export async function postProduct(  fakeProduct: FakeProduct): Promise<any> {
+  const response = await callPostHttpAdapter(`${baseUrl}/products}`, fakeProduct);
+  return response;
+}
+
+export async function deleteProduct(id: string): Promise<any> {
+  const response = await callDeleteHttpAdapter(`${baseUrl}/products/${id}`);
+  return response;
+}
